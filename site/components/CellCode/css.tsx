4486af66970e9f3478841aae8fe294ab46dba484
cc <44751363+zzfn@users.noreply.github.com>
2024-02-06 17:46:06 +0800

    style: tree

--- site/components/CellCode/css.tsx
+++
@@ -1,5 +0,0 @@
-function CellCodeCss({ children }: { children: React.ReactNode }) {
-  return <div className="oc-code-wrapper-css">{children}</div>;
-}
-
-export default CellCodeCss;
--- site/components/CellCode/index.tsx
+++ site/components/CellCode/index.tsx
@@ -1,132 +1,45 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
-import { IconButton, Message, Space, Tooltip } from '@oc/design';
+import { type PropsWithChildren, type ReactNode, useRef, useState } from 'react';
+import { IconButton, Space, Tooltip } from '@oc/design';
 import { IconCode, IconCopy } from '@oc/icon';
-import ClipboardJS from 'clipboard';
-import { motion, AnimatePresence } from 'framer-motion';
-import Css from './css';
 
-interface CellCodeProps {
-  tsx?: ReactNode;
-  cssCode?: string;
+function CellCode(props: PropsWithChildren<any>) {
+  const { children } = props;
+  const ins = useRef<HTMLDivElement>(null);
+  const [isShow, setIsShow] = useState(false);
+  return (
+    <div ref={ins}>
+      <Space>
+        <Tooltip content="展开代码">
+          <IconButton border onClick={() => setIsShow(!isShow)}>
+            <IconCode />
+          </IconButton>
+        </Tooltip>
+        <Tooltip content="复制代码">
+          <IconButton
+            onClick={() => {
+              const clipboardObj = navigator.clipboard;
+              clipboardObj.writeText(
+                (ins.current?.querySelector('.language-js') as HTMLElement).innerText
+              );
+            }}
+            border
+          >
+            <IconCopy />
+          </IconButton>
+        </Tooltip>
+      </Space>
+      <div
+        style={{
+          display: isShow ? 'block' : 'none',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 
-interface CellCodeState {
-  showAll: boolean;
-  codeType: 'jsx' | 'tsx';
-}
-
-const CODE_JSX = 'jsx';
-const CODE_TSX = 'tsx';
-
-const locales = {
-  'zh-CN': {
-    copy: '复制',
-    copied: '复制成功',
-    expand: '展开代码',
-    collapse: '收起代码',
-  },
+CellCode.Css = function CellCss({ children }: { children: ReactNode }) {
+  return <div>{children}</div>;
 };
-
-class CellCode extends React.Component<PropsWithChildren<CellCodeProps>, CellCodeState> {
-  private btnCopy = null;
-
-  private codeEle = null;
-
-  constructor(
-    props: React.PropsWithChildren<CellCodeProps> | Readonly<React.PropsWithChildren<CellCodeProps>>
-  ) {
-    super(props);
-    this.state = {
-      showAll: false,
-      codeType: props.tsx ? CODE_TSX : CODE_JSX,
-    };
-  }
-
-  componentDidMount(): void {
-    const t = locales['zh-CN'];
-    if (this.btnCopy) {
-      const clipboard = new ClipboardJS(this.btnCopy, {
-        text: () =>
-          ((this.codeEle as never as HTMLElement).querySelector('.language-js') as HTMLElement)
-            .innerText,
-      });
-      clipboard.on('success', (e) => {
-        e.clearSelection();
-        Message.success(t.copied);
-      });
-    }
-  }
-
-  toggleCode = (e: any) => {
-    // 修正点击展开按钮时，页面向上滚动而不是向下滚动的问题
-    if (!this.state.showAll) {
-      e.target.blur();
-    }
-    this.setState({
-      showAll: !this.state.showAll,
-    });
-  };
-
-  renderOperations = () => {
-    const { showAll } = this.state;
-    const t = locales['zh-CN'];
-
-    return (
-      <div className="arco-code-operations">
-        <Space>
-          <Tooltip content={showAll ? t.collapse : t.expand}>
-            <span>
-              <IconButton border onClick={this.toggleCode} aria-label={t.collapse}>
-                <IconCode />
-              </IconButton>
-            </span>
-          </Tooltip>
-          <Tooltip content={t.copy}>
-            <span>
-              <IconButton border ref={(ref: any) => (this.btnCopy = ref)} aria-label={t.copy}>
-                <IconCopy />
-              </IconButton>
-            </span>
-          </Tooltip>
-        </Space>
-      </div>
-    );
-  };
-
-  render() {
-    const { props } = this;
-    const { showAll, codeType } = this.state;
-    return (
-      <div className="arco-code-wrapper">
-        {this.renderOperations()}
-        <AnimatePresence initial={false}>
-          {showAll && (
-            <motion.div
-              key="content"
-              initial="collapsed"
-              animate="open"
-              exit="collapsed"
-              variants={{
-                open: { opacity: 1, height: 'auto' },
-                collapsed: { opacity: 0, height: 0 },
-              }}
-              transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
-            >
-              <div className="content-code-design show-all">
-                <div className="code" ref={(ref: any) => (this.codeEle = ref)}>
-                  {codeType === CODE_TSX ? props.tsx : props.children}
-                </div>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
-    );
-  }
-}
-
-type CellCodeType = typeof CellCode & { Css: any };
-
-(CellCode as CellCodeType).Css = Css;
-
-export default CellCode as CellCodeType;
+export default CellCode;
